9983566e6aae7877a5cfd0ba75d82f55cf13c317
Vadim Kazimirov <v.kazimirov@g.nsu.ru>
2023-04-29 17:59:10 +0700

    added extra test for all classes from Classes.h + new task4 for Part 3 (on site)

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "Container.h"
 #include "iostream"
+#include "Classes.h"
 
 void test1() {      // get/add test
     Container<int> test1;
@@ -55,10 +56,82 @@ void test3() {      // reverse test
     }
 }
 
+void test4() {
+    Container<Task*> containerWithTasks;
+    Container<std::string> containerInfo;
+    containerWithTasks.addLast(new BinaryCalculation(12312, '+', 138912));
+    containerWithTasks.addLast(new AddTaskToContainer(&containerWithTasks, new BinaryCalculation(1, '+', 1)));
+    containerWithTasks.addLast(new AddTaskToContainer(&containerWithTasks, new BinaryCalculation(1, '-', 1)));
+    containerWithTasks.addLast(new AddTaskToContainer(&containerWithTasks, new BinaryCalculation(1, '*', 1)));
+    containerWithTasks.addLast(new AddTaskToContainer(&containerWithTasks, new BinaryCalculation(1, '/', 10)));
+    containerWithTasks.addLast(new AddTaskToContainer(&containerWithTasks, new BinaryCalculation(4, '/', 2)));
+    containerWithTasks.addLast(new AddTaskToContainer(&containerWithTasks, new BinaryCalculation(4, '*', 0)));
+    containerWithTasks.addLast(new AddTaskToContainer(&containerWithTasks, new BinaryCalculation(5, '/', 0)));
+    containerWithTasks.addLast(new AddTaskToContainer(&containerWithTasks, new BinaryCalculation(6, '-', 1)));
+    containerWithTasks.addLast(new AddTaskToContainer(&containerWithTasks, new BinaryCalculation(7, '-', 1)));
+    containerWithTasks.addLast(new AddTaskToContainer(&containerWithTasks, new BinaryCalculation(12, '*', 13)));
+    containerWithTasks.addLast(new AddTaskToContainer(&containerWithTasks, new BinaryCalculation(51, '/', 4)));
+
+    NumberOfObjects* tasksCounter = new NumberOfObjects;
+    tasksCounter->apply();
+    std::cout << tasksCounter->toString() << std::endl;
+
+    while(!containerWithTasks.isEmpty()){
+        Task* task = containerWithTasks.popFirst();
+        task->apply();
+        containerInfo.addLast(task->toString());
+        delete task;
+    }
+
+    for (auto iterator = containerInfo.begin(); iterator!= ++containerInfo.end(); ++iterator){
+        std::cout << (*iterator) << std::endl;
+    }
+
+    delete tasksCounter;
+
+    std::cout << Object::getCount() << std::endl;
+
+} // Task3 on site
+
+void test5() {
+
+    Container<Task*> containerWithTasks;
+
+    containerWithTasks.addLast(new AddTaskToContainer(&containerWithTasks, new BinaryCalculation(40, '*', 10)));
+    containerWithTasks.addLast(new BinaryCalculation(1456, '-', 123));
+
+    ClearContainer customTask1(&containerWithTasks);
+    NumberOfObjects customTask2;
+
+    NumberOfTasksWithResult customTask3(&containerWithTasks);
+
+    customTask2.apply();
+    std::cout << customTask2.getResult() << std::endl;
+    std::cout << containerWithTasks.size() << std::endl;
+
+    customTask3.apply();
+    ObjectsInContainer customTask4 (&containerWithTasks);
+    customTask4.apply();
+    std::cout << customTask3.toString() << std::endl;
+    std::cout << customTask4.toString() << std::endl;
+
+    customTask1.apply();
+    customTask2.apply();
+    std::cout << customTask2.getResult() << std::endl;
+    std::cout << containerWithTasks.size() << std::endl;
+
+    containerWithTasks.addLast(new ClearContainer(&containerWithTasks));
+    containerWithTasks.getFirst()->apply();
+    std::cout << containerWithTasks.size() << std::endl;
+    
+}
+
 int main() {
 
-    test1();
+//    test1();
 //    test2();
 //    test3();
+//    test4();
+    test5();
 
 }
